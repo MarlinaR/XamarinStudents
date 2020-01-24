63aa23c2d6e33fe1bffaa00316459c34e4aefc4c
ArinaRaguzina <raguzina.aa@students.dvfu.ru>
2020-01-24 15:58:49 +1000

    Extra_task_1

---
+++ Extra_task_1/ClassColor.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Drawing;
+using System.Reflection;
+
+namespace Extra_task_1
+{
+    class ClassColor : Xamarin.Forms.Xaml.IMarkupExtension
+    {
+        public int A { get; set; }
+        public int R { get; set; }
+        public int G { get; set; }
+        public int B { get; set; }
+
+        public object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return Xamarin.Forms.Color.FromRgba(R, G, B, A);
+        }
+    }
+}
---
+++ Extra_task_1/MainPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Extra_task_1
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        ObservableCollection<ClassListColor> Colors { get; set; }
+
+        public MainPage()
+        {
+            InitializeComponent();
+            Colors = new ObservableCollection<ClassListColor>();
+            foreach (System.Drawing.Color color in ClassListColor.GetColors())
+            {
+                Colors.Add(new ClassListColor { name = color.Name, color = color }); 
+
+            }
+            listView.ItemsSource = Colors;
+            //Label header = new Label
+            //{
+            //    Text = "Список моделей",
+            //    FontSize = Device.GetNamedSize(NamedSize.Large, typeof(Label)),
+            //    HorizontalOptions = LayoutOptions.Center
+            //};
+            //ListView listView = new ListView();
+            //// определяем источник данных
+            //listView.ItemsSource = Colors;
+            //this.Content = new StackLayout { Children = { header, listView } };
+
+        }
+    }
+}
